fef5f9747d3b6aabc3256ecc0e2bfd1758fe8d77
Felipe Azevedo Bosi <bosiarquitetura@gmail.com>
2022-11-30 12:33:17 -0300

    Estudos do dia 30/11/2022

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -4,19 +4,31 @@ class NegociacaoController {
     this._inputData = $("#data");
     this._inputQuantidade = $("#quantidade");
     this._inputValor = $("#valor");
+    this._negociacoes = new Negociacoes();
   }
 
   adiciona(event) {
     event.preventDefault();
-    let converter = new DateConverter();
-    let data = converter.paraData(this._inputData.value);
-    let negociacao = new Negociacao(
-      data,
+
+    const negociacao = this._criarNegociacao();
+    this._negociacoes.adiciona(negociacao);
+    console.log(this._negociacoes.paraArray());
+
+    this._limpaFormulario();
+  }
+
+  _criarNegociacao() {
+    return new Negociacao(
+      DateConverter.paraData(this._inputData.value),
       parseInt(this._inputQuantidade.value),
       parseFloat(this._inputValor.value)
     );
+  }
 
-    let diaMesAno = converter.paraTexto(negociacao.data);
-    console.log(diaMesAno);
+  _limpaFormulario() {
+    this._inputData.value = "";
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0.0;
+    this._inputData.focus();
   }
 }
--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -1,11 +1,17 @@
 class DateConverter {
-  paraTexto(data) {
-    return (
-      data.getDate() + "/" + (data.getMonth() + 1) + "/" + data.getFullYear()
-    );
+  constructor() {
+    throw new Error("Esta classe não deve ser instanciada");
   }
 
-  paraData(text) {
+  static paraTexto(data) {
+    return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
+  }
+
+  static paraData(text) {
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(text)) {
+      throw new Error("A data deve estar no formato aaaa-mm-dd");
+    }
+
     return new Date(
       ...text.split("-").map((item, index) => item - (index % 2))
     );
